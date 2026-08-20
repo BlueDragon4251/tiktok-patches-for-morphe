ba25c0543a50887ba8933895f1735b284c498901
BlueDragon4251 <136262109+BlueDragon4251@users.noreply.github.com>
2026-08-20 21:43:38 +0200

    refactor(tiktok): remove legacy TikTok settings fragment name

--- extensions/tiktok/src/main/java/app/morphe/extension/tiktok/settings/preference/TikTokPreferenceFragment.java
+++
@@ -1,9 +0,0 @@
-package app.morphe.extension.tiktok.settings.preference;
-
-/**
- * Legacy compatibility alias. Active settings implementation is BlueITPreferenceFragment.
- */
-@Deprecated
-@SuppressWarnings("deprecation")
-public class TikTokPreferenceFragment extends BlueITPreferenceFragment {
-}
